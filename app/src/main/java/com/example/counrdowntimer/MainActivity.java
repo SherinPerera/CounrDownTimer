2cfaafce94d9d9b82da02032f2ca0e7f0ac784e8
SherinPerera <sherinperera42@gmail.com>
2019-03-06 14:33:51 +0530

    Implemented a handler

--- app/src/main/java/com/example/counrdowntimer/MainActivity.java
+++ app/src/main/java/com/example/counrdowntimer/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.counrdowntimer;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private int counter =99;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        countDown();
+    }
+
+    private void countDown(){
+        final TextView textView2 =findViewById(R.id.textView2);
+        final Handler handler=new Handler();
+        Handler.post(new Runnable() {
+            @Override
+            public void run() {
+                textView2.setText(Integer.toString(counter));
+                counter--;
+                handler.PostDelayed(this,1000);
+
+            }
+        })
+
+
     }
 }
