100bfcc359823100ebf1d564e2365545219eb265
SherinPerera <sherinperera42@gmail.com>
2019-03-11 09:26:15 +0530

    reset the number

--- app/src/main/java/com/example/counrdowntimer/MainActivity.java
+++ app/src/main/java/com/example/counrdowntimer/MainActivity.java
@@ -6,23 +6,38 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private int counter =99;
-
+    private int countDown =99;
+    private static final String CURRENT_COUNTER ="counter";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(savedInstanceState != null){
+            countDown=savedInstanceState.getInt(CURRENT_COUNTER);
+        }
+
         countDown();
+    }
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt(CURRENT_COUNTER,countDown);
+
     }
 
     private void countDown(){
-        final TextView textView2 =findViewById(R.id.textView2);
+
         final Handler handler=new Handler();
         handler.post(new Runnable() {
             @Override
             public void run() {
-                textView2.setText(Integer.toString(counter));
-                counter--;
+                final TextView textView2 =findViewById(R.id.textView2);
+                textView2.setText(Integer.toString(countDown));
+                //Reset the counter to avoid getting minus numbers
+                if(countDown == 0){
+                    countDown = 99;
+                }
+                countDown--;
                 handler.postDelayed(this,1000
                 );
 
